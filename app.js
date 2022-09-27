d56b23cb206cb91dd46024e205857bc2e67c037d
Aman <bhandari.aman0101@gmail.com>
2022-09-27 13:01:25 +0530

    Basic routes and middleware setup

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+require('express-async-errors')
 
 // express
 const express = require('express')
@@ -6,8 +7,20 @@ const app = express()
 
 //database
 const connectDB = require('./db/connect')
-const port = process.env.port || 5000
 
+//middleware
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('e-commerce app')
+})
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.port || 5000
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL)
