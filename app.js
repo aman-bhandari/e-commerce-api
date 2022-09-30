c50c294f0eee3cca00e9b66c7054bbd5a1164905
Aman <bhandari.aman0101@gmail.com>
2022-09-30 20:26:42 +0530

    user routes structure

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const connectDB = require('./db/connect')
 
 //router
 const authRouter = require('./routes/authRoutes')
-
+const userRouter = require('./routes/userRoutes')
 //middleware
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
@@ -28,6 +28,7 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/users', userRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
