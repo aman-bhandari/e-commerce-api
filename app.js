3422f0a638e1dc554688da0631d8263b154ba58e
Aman Bhandari <bhandari.aman0101@gmail.com>
2022-10-16 00:42:03 +0530

    order setup

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ const authRouter = require('./routes/auth-routes')
 const userRouter = require('./routes/user-routes')
 const productRouter = require('./routes/product-routes')
 const reviewRouter = require('./routes/review-routes')
+const orderRouter = require('./routes/order-routes')
+
 //middleware
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
@@ -35,6 +37,7 @@ app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/products', productRouter)
 app.use('/api/v1/reviews', reviewRouter)
+app.use('/api/v1/orders', orderRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
